46a0a8e1ff85357e8c08ecb6027586400366f713
bmg <bharatgupta87567@gmail.com>
2025-02-07 00:40:08 +0530

    Changes

--- v02_VarAndDatatypes.java
+++ v02_VarAndDatatypes.java
@@ -1,6 +1,7 @@
 public class v02_VarAndDatatypes {
     public static void main(String[]args){
         System.out.println("Variable And DataTypes");
+        System.out.println("Variable And DataTypes");
     }
 
 }

cd61f9cc2e1605170d5db9c00cb28dc13a1e4c9e
bmg <bharatgupta87567@gmail.com>
2025-02-14 12:48:57 +0530

    New Method Added

--- v07_String.java
+++ v07_String.java
@@ -1,8 +1,33 @@
 public class v07_String {
     public static void main(String[]args){
-        // Strings Is Sequence Of Charcter
-        String name = new String("Bhaat");
-        System.out.println(name);
-        
+        // Strings Is Sequence Of Charcter And String Are Immuttable And Cannot Be Changed.
+        // Strings Method
+         String name = "BharatGupta , bbxjsbux";
+        //  System.out.println(name.length());    // For Length Of String Value
+
+        //  String lstring = name.toLowerCase();  // To Change Into Lowercase Of String Value
+        //  System.out.println(lstring);
+
+        //  String ustring = name.toUpperCase();  // To Change Into Uppercase Of String Value
+        //  System.out.println(ustring);
+       
+        //  String name1 = "    Bharat     ";
+        //  String trim = name1.trim();          // Trim Method Uesd In String To Remove Space
+        //  System.out.println(trim);
+
+        //  System.out.println(name.substring(2));  // Used To Print From Indexing Value
+
+        //  System.out.println(name.substring(0,11)); // Used To Print From Given Value 
+
+        //  System.out.println(name.replace('B', 'P'));   // Used To Replace  Single Word
+        //  System.out.println(name.replace("Bharat", "Pallu"));   // Used To Replace  Whole Word
+
+        // System.out.println(name.startsWith("Bhar")); // Used To Check Value Start With Boolean
+        // System.out.println(name.endsWith("ta"));     // Used To Check Value Ends With Boolean
+
+        System.out.println(name.charAt(5));    // Used To Check Character With Index Value
+
+        System.out.println();
+
     }
 }

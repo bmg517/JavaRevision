eab7f2ff141deb759eaa642885649152e131d26c
bmg <bharatgupta87567@gmail.com>
2025-02-14 16:14:05 +0530

    Added

---
+++ v09_ConditionInJava.java
@@ -0,0 +1,21 @@
+public class v09_ConditionInJava {
+    public static void main (String[]args){
+        // Decision Making Instruction In Java
+        // 1. If-Else Statement
+        // 2. Switch Statement
+
+// 1. If-Else Statement
+       //   int age = 19;
+       //   if (age>18){
+       //     System.out.println("You Can Drive");
+       //   }
+       //   else{
+       //     System.out.println("No, You Cannot Deive");
+       //   }
+
+// 2. Switch Statement
+
+
+
+    }
+}
---
+++ v10_LoopInJava.java
@@ -0,0 +1,18 @@
+public class v10_LoopInJava {
+    public static void main(String[]args){
+    // 1. While Loop
+    // 2. Do-While Loop
+    // 3. For Loop
+
+        int a = 2;
+        while(a>1){
+            System.out.println(a);
+            a++;
+        }
+
+
+
+    
+
+    }
+}

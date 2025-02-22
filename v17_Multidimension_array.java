3d69bc256fde8aa8da1a17efdad12d3839dde4e2
bmg <bharatgupta87567@gmail.com>
2025-02-23 00:40:16 +0530

    Added

--- v17_Multidimension_array.java
+++ v17_Multidimension_array.java
@@ -1,8 +1,24 @@
 public class v17_Multidimension_array {
     public static void main(String[]args){
-    // Multidimensional Array is a Array Of Array
-    int [] marks = {100,90,80,70};
-    System.out.println(marks[0]);
+        // Multidimensional Array is a Array Of Array
+        int [] mark;   // 1-D Arrays
+        int [][] flats;    // 2-D Arrays
+        flats = new int [2][3];
+        flats[0][0]=101;
+        flats[0][1]=102;
+        flats[0][2]=103;
+        flats[1][0]=201;
+        flats[1][1]=202;
+        flats[1][2]=203;
     
+        for( int i =0;i<flats.length;i++ ){
+             for(int j=0; j<flats[i].length; j++){
+                System.out.print(flats[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    
+
     }
 }    
